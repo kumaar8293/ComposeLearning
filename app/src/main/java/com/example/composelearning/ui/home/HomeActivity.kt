d63f1dd6846ff566b3243ff2e79e3a22fcc469e2
Mukesh Kumar Patel <mukesh.patel@lenskart.in>
2024-05-08 12:48:39 +0530

    Simple user card UI https://prnt.sc/bNq5zpnebAUn

--- app/src/main/java/com/example/composelearning/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/composelearning/ui/home/HomeActivity.kt
@@ -4,15 +4,33 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composelearning.R
 
@@ -22,7 +40,8 @@ class HomeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         // Roughly equivalent to calling ComponentActivity.setContentView() with a ComposeView i.e.:
         setContent {
-           GreetingMessage(msg = "Nishant Brokerr")
+            // GreetingMessage(msg = "Nishant Brokerr")
+            SimpleUserCard()
         }
     }
 
@@ -45,11 +64,56 @@ class HomeActivity : ComponentActivity() {
         )
     }
 
+    // =========== STEP 2 ==============
+    @Composable
+    fun SimpleUserCard() {
+        //User for Horizontal Parent
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(12.dp)
+                .border(1.dp, Color.Gray)
+                .padding(12.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.sample),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape)
+                    .align(Alignment.CenterVertically)
+
+            )
+            val context = LocalContext.current
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(text = stringResource(id = R.string.sample_string))
+                Button(
+                    onClick = {
+                        Toast.makeText(context, "Ki Haal Chall Praa", Toast.LENGTH_SHORT).show()
+                    }
+                ) {
+                    Text(text = "View Profile")
+                }
+            }
+        }
+
+    }
+
     //Preview is only use for the UI preview.
     @Preview(showBackground = true)
     @Composable
     fun Preview() {
-        GreetingMessage(msg = "Sam Boxer")
+        /** 1st component **/
+        //GreetingMessage(msg = "Sam Boxer")
+
+        /** 2nd component **/
+        SimpleUserCard()
     }
 
 }
